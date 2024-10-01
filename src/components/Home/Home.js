372065087ae30d1c27f0ad997e5e08c825b31a0d
Your Name <you@example.com>
2024-10-01 11:58:53 +0900

    12

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom' 
+import React from "react";
+import { Link } from "react-router-dom";
+import Img from "../../Assets/pentagon.png";
+import Cart from "../../Assets/cartt.png";
+import "./Home.css";
 
- function Home() {
+function Home() {
   return (
     <div>
-        <button type='submit'> <Link to = '/login'>Login</Link> </button>
-        <button type='submit'> <Link to = '/signup'>Signup</Link></button>
+      <div className="Navbar">
+        <img src={Img} alt="" className="logo" />
+        <input
+          type="text"
+          placeholder="search"
+          className="search"
+          name="text"
+        />
+        <button type="button" className="Buttonn">
+          your orders
+        </button>
+        <img src={Cart} alt="" className="cart" />
+        <button type="button" className="Button">
+          sign in here
+        </button>
+      </div>
+
+      <div className="Container"></div>
     </div>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,69 +3,69 @@ import './Login.css'
 import React, { useState } from 'react'
 import { FaUser } from "react-icons/fa6"
 import { FaLock } from "react-icons/fa6"
-import {auth} from '../firebase/firebase'
-import { signInWithEmailAndPassword} from 'firebase/auth'
+import { auth } from '../firebase/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 import { Navigate } from 'react-router-dom'
 
 
-    const Login = () => {
+const Login = () => {
 
-        const [email,setEmail] = useState ('')
-        const [password,setPassword] = useState('')
-        
-        const handleSubmit = (e) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-            e.preventDefault();
-       signInWithEmailAndPassword(auth, email,password)
-        .then((userCredential) => {
-           console.log(userCredential)
-           Navigate("/main")
-        })
-        .catch((error) =>{
-            console.log(error);
-        });
-        } 
+    const handleSubmit = (e) => {
 
-        
-  return (
-    
-  <div className='Container'>
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential)
+                Navigate("/main")
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-    <div className='Form'>
-        <form  onSubmit={handleSubmit}>
-            <h1>Login</h1>
-            <div className='form-group'>
-                <label for ="username">Username</label><br />
-                <FaUser size='0.9rem' /><input type = 'text' 
-                placeholder='write your username' name='usename'  
-                onChange={(e) => setEmail(e.target.value)} />
+
+    return (
+
+        <div className='Container'>
+
+            <div className='Form'>
+                <form onSubmit={handleSubmit}>
+                    <h1>Login</h1>
+                    <div className='form-group'>
+                        <label for="username">Username</label><br />
+                        <FaUser size='0.9rem' /><input type='text'
+                            placeholder='write your username' name='usename'
+                            onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+
+                    <div className='form-group'>
+                        <label for="password">Password</label><br />
+                        <FaLock size='0.9rem' /> <input type='password'
+                            placeholder='write password' name='password'
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='forget'>
+                        <p>forget you password</p>
+                    </div>
+
+                    <button type='submit'>Login</button>
+
+                    <div className='Different'>
+                        <p>Or signup using</p>
+                    </div>
+                    <div className='Signup'>
+                        <p>Signup</p>
+                    </div>
+
+                </form>
             </div>
-
-            <div className='form-group'>
-                <label for="password">Password</label><br />
-                <FaLock size='0.9rem' /> <input type='password' 
-                placeholder='write password' name='password'
-                 onChange={(e) => setPassword(e.target.value)}  />
-            </div>
-            <div className='forget'>
-                <p>forget you password</p>
-            </div>
-
-                <button type='submit'>Login</button>
-         
-            <div className='Different'>
-                <p>Or signup using</p>
-            </div>
-            <div className='Signup'>
-                <p>Signup</p>
-            </div>
-
-        </form>
         </div>
-    </div>
-     
 
-  )
+
+    )
 }
 
 export default Login
